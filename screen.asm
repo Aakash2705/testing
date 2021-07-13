b1f2e1f80ec368e076fe4d4bb93409887ea66377
Aakash2705 <aakashbose2705@gmail.com>
2021-07-13 12:30:00 +0530

    chnaged stuff in screen.asm changed some registers some label names

--- screen.asm
+++ screen.asm
@@ -20,7 +20,7 @@ puts:
 
 [bits 32]
 ; define some constants
-VIDEO_MEMORY equ 0xb80000
+VIDEO_MEMORY equ 0xb8000
 WHITE_ON_BLACK equ 0x0f
 
 ; prints a null-terminated string pointed by EDX
@@ -33,12 +33,12 @@ print_string_pm_loop:
 	mov ah, WHITE_ON_BLACK
 
 	cmp al, 0
-	je done
+	je print_string_pm_done
 
 	mov [edx], ax
 	
 	add ebx, 1
-	add ebx, 2
+	add edx, 2
 
 	jmp print_string_pm_loop
 
